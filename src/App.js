bc43877cd7a9cb98dce6cdec4e307f6ad38c7f07
g00410404 <G00410404@atu.ie>
2023-10-06 14:32:57 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Content from './components/content';
+import Header from './components/header';
+import Footer from './components/footer';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {  /* <Header></Header>
+        <Content></Content>
+        <Footer></Footer>*/}
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+            <Nav.Link href="/Read">Read</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>}> </Route>
+        <Route path='/create' element={<Footer></Footer>}> </Route>
+        <Route path='/read' element={<Header></Header>}> </Route>
+      </Routes>
     </div>
   );
 }
